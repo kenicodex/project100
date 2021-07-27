4eb646c5bfbcb20fbeededfe149497e46c466a94
kehinde <kehindesalaudeen222@gmail.com>
2021-07-27 11:40:17 +0100

    links

--- src/Homepage/Navbar.jsx
+++ src/Homepage/Navbar.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import './style.css'
 
 function Navbar(props) {
+  
+// Work with us 
+// Employment form 
+// Contact us 
+// What we do 
+// Mission 
+// Vision
   return (
     <div style={{ height: "100px" }} className="position-relative">
       <div className="position-absolute p-2 text-center text-dark" style={{ height: "80px", font: "30px open", left: "95px", top: "22px" }}>
@@ -9,7 +17,11 @@ function Navbar(props) {
         <div style={{ font: "15px open", letterSpacing: "2px" }}> Canada</div>
       </div>
       <div style={{ width: "", left: "388px", top: "30px" }} className="position-absolute">
-        <div>Home</div>
+        <a href="/" > Home</a>
+        <a href="/" > Employment form </a>
+        <a href="/" > Contact us</a>
+        <a href="/" > What we do </a>
+        <a href="/" > Mission and Vision</a>
       </div>
 
     </div>
